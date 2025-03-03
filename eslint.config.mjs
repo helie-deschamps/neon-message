0fa076f76bc65b0fa1b68edae213bd4104d1e05e
Hélie <bonjour@helie.me>
2025-03-03 17:38:09 +0100

    Add Prettier and ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-config-prettier"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import { FlatCompat } from "@eslint/eslintrc"
+import eslintPluginUnicorn from "eslint-plugin-unicorn"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	prettier,
+	eslintPluginUnicorn.configs.recommended,
+	{
+		rules: {
+			"unicorn/filename-case": [
+				"error",
+				{
+					cases: {
+						camelCase: true,
+						pascalCase: true,
+					},
+					multipleFileExtensions: false,
+				},
+			],
+			"unicorn/number-literal-case": "off",
+		},
+	},
+]
 
-export default eslintConfig;
+export default eslintConfig
