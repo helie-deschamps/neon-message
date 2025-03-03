f00b90dc0ebada8c93fe4e42858d894193fa2412
Hélie <bonjour@helie.me>
2025-03-03 18:40:23 +0100

    Patch a bug causing non exclusion of node module by linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import path from "node:path"
 import { fileURLToPath } from "node:url"
 import { FlatCompat } from "@eslint/eslintrc"
 import eslintPluginUnicorn from "eslint-plugin-unicorn"
+import { includeIgnoreFile } from "@eslint/compat"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -10,8 +11,10 @@ const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
 })
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url))
 
 const eslintConfig = [
+	includeIgnoreFile(gitignorePath),
 	...compat.extends("next/core-web-vitals", "next/typescript"),
 	prettier,
 	eslintPluginUnicorn.configs.recommended,
