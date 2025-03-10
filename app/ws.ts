d97a387f5f0e892c2794c30ee832d6ca53a33bbc
Hélie <bonjour@helie.me>
2025-03-10 17:41:05 +0100

    Separate onConnection to different file

--- app/ws.ts
+++ app/ws.ts
@@ -1,10 +1,5 @@
 import { Server } from "socket.io"
-import changeUsername from "./events/changeUsername"
-import usersList from "./usersList"
-import newUser from "./events/newUser"
-import startTyping from "./events/startTyping"
-import endTyping from "./events/endTyping"
-import sendingMessage from "./events/sendingMessage"
+import onConnection from "./onConnection"
 
 const io = new Server(3000, {
 	cors: {
@@ -13,17 +8,4 @@ const io = new Server(3000, {
 	},
 })
 
-io.on("connection", socket => {
-	const publicId = socket.id
-
-	newUser(socket, publicId)
-
-	socket.on("change_username", (data: string) => changeUsername(data, publicId))
-	socket.on("start_typing", () => startTyping(publicId))
-	socket.on("end_typing", () => endTyping(publicId))
-	socket.on("sending_message", (data: string) => sendingMessage(data, publicId))
-
-	socket.on("disconnect", () => {
-		delete usersList[publicId]
-	})
-})
+io.on("connection", onConnection)
